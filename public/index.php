ca6b935045a72f20b552f7ea65db59608a94146a
皮泽培 <848733913@qq.com>
2018-07-26 14:39:07 +0800

    创建文件

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @Author: pizepei
+ * @Date:   2018-07-26 14:34:25
+ * @Last Modified by:   pizepei
+ * @Last Modified time: 2018-07-26 14:35:10
+ */
+require('../vendor/autoload.php');
