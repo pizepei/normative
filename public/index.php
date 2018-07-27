d01ce9086b4966a19e753e461c2c8b3ea9077bf3
皮泽培 <848733913@qq.com>
2018-07-27 18:04:10 +0800

    简单的代码

--- public/index.php
+++ public/index.php
@@ -9,3 +9,16 @@
 header("Content-Type=text/html;charset=utf8");
 
 require('../vendor/autoload.php');
+
+echo '<pre>';
+//var_dump(pizepei\config\Dbtabase::DBTABASE);
+
+//pizepei\model\cache\Cache::set('CONFIG',pizepei\config\Dbtabase::DBTABASE);
+pizepei\model\cache\Cache::set('PPX',pizepei\config\Dbtabase::DBTABASE);
+
+pizepei\model\db\Db::table('user')->showCreateTableCache();
+
+
+
+
+echo '</pre>';
