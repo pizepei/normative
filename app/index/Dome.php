b4e225f33e6dd23d5610d5fb4a9a117341e9e805
pizepei <848733913@qq.com>
2019-02-08 23:23:58 +0800

    redis

--- app/index/Dome.php
+++ app/index/Dome.php
@@ -31,7 +31,7 @@ class Dome
 {
 
     /**
-     * @return array [object]
+     * @return array [html]
      * @title  / 路由的应用
      * @explain 注意所有 path 路由都使用 正则表达式为唯一凭证 所以 / 路由只能有一个
      * @router get /index.html
