707e62673bb091b689e4eed6242f837fb306b5e9
皮泽培 <848733913@qq.com>
2019-04-01 21:42:10 +0800

    规划文件上传

--- app/OpenCommon.php
+++ app/OpenCommon.php
@@ -18,5 +18,57 @@ use pizepei\staging\Request;
 class OpenCommon extends Controller
 {
 
+    /**
+     * @param \pizepei\staging\Request $Request [xml]
+     *      path [object] 路径参数
+     *      post [object]
+     * @return array [json]
+     * @title  文件上传
+     * @explain 文件上传（公开的但是会判断上传域名）
+     * @router post files-upload
+     * @throws \Exception
+     */
+    public function openMessage(Request $Request)
+    {
+        /**
+         * 获取当前域名
+         */
+        $HOST = $_SERVER['HTTP_HOST'];
+
+        /**
+         * 处理文件
+         */
+
+        foreach($_FILES as $key=>$value){
+
+
+
+
+
+        }
+
+
+
+
+        /**
+         * 获取文件大小
+         */
+
+
+        /**
+         * 获取文件类型
+         */
+
+
+        /**
+         * 拼接文件
+         */
+
+        var_dump( $_SERVER['HTTP_HOST'] );
+        var_dump($_FILES);
+
+        return $Request->input('','raw');
+    }
+
 
 }
