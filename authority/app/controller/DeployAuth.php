5a5cdb6943ba0b35acfff67524609a1b91202424
pizepei <848733913@qq.com>
2019-06-30 23:01:27 +0800

    完善部署

---
+++ authority/app/controller/DeployAuth.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @Author: pizepei
+ * @ProductName: PhpStorm
+ * @Created: 2019/6/16 11:56 $
+ * @title 简单的dome
+ * @explain 类的说明
+ */
+
+namespace authority\app\controller;
+
+use pizepei\staging\Authority;
+
+class DeployAuth extends Authority
+{
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    public function test()
+    {
+        return[];
+    }
+}
