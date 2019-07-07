d22e380bfcd2a4d0db48d60149646744b9c3691e
pizepei <848733913@qq.com>
2019-07-07 22:29:54 +0800

    账号管理

--- app/index/Account.php
+++ app/index/Account.php
@@ -13,10 +13,9 @@
 
 namespace app\index;
 
-use basics\model\account\AccountModel;
-use basics\model\account\AccountThirdPartyModel;
-use pizepei\model\db\Db;
 use pizepei\staging\Controller;
+use pizepei\staging\Request;
+use service\basics\account\AccountService;
 
 class Account extends Controller
 {
@@ -31,6 +30,38 @@ class Account extends Controller
     {
         //Db::table()->insert();
         return AccountThirdPartyModel::table()->add(['status'=>3,'type'=>'WeChat','openid'=>'77779676','account_id'=>[Db::getUuid()]]);
+    }
+
+
+    /**
+     * @Author pizepei
+     * @Created 2019/7/5 22:40
+     * @param \pizepei\staging\Request $Request
+     *      post [object]
+     *          phone [string number] 手机号码
+     *          code [string required] 手机验证码
+     *          email [string email] 邮箱
+     *          password [string required] 密码
+     *          repass [string required] 确认密码
+     *          nickname [string required] 昵称
+     *          agreement [string required] 是否同意协议
+     * @title  注册接口
+     * @explain 获注册接口
+     * @throws \Exception
+     * @return array [json]
+     *      data [raw]
+     * @router post account
+     */
+    public function registerAccount(Request $Request)
+    {
+        $Service = new AccountService();
+        $res = $Service->register(\Config::ACCOUNT,$Request->post());
+        if($res['result'])
+        {
+            return $this->succeed('',$res['msg']);
+        }else{
+            return $this->error('',$res['msg']);
+        }
     }
 
 
