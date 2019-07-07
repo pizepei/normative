e8a26eced368e2f94f0b5f127553072192ba1a0e
pizepei <848733913@qq.com>
2019-07-07 13:32:10 +0800

    完善部署

--- app/Deploy.php
+++ app/Deploy.php
@@ -13,6 +13,8 @@ namespace app;
 
 
 use pizepei\deploy\DeployService;
+use pizepei\deploy\LocalDeployServic;
+use pizepei\func\Func;
 use pizepei\model\db\TableAlterLogModel;
 use pizepei\staging\Controller;
 use pizepei\staging\Request;
@@ -42,7 +44,7 @@ class Deploy extends Controller
      * @Author pizepei
      * @Created 2019/6/12 22:39
      * @param \pizepei\staging\Request $Request
-     *
+
      * @title  删除本地配置接口
      * @explain 当接口被触发时会删除本地所有Config配置，配置会在项目下次被请求时自动请求接口生成
      * @router delete Config
@@ -50,20 +52,43 @@ class Deploy extends Controller
     public function deleteConfig(Request $Request)
     {
 
+        Func::M('file')::deldir();
+
     }
+
     /**
      * @Author pizepei
      * @Created 2019/6/12 22:43
      * @param \pizepei\staging\Request $Request
-     *      path [object] 路径参数
+     *      raw [object] 路径
      *          path [string] 需要删除的runtime目录下的目录为空时删除runtime目录
      * @title  删除本地runtime目录下的目录
      * @explain 删除runtime目录下的目录或者runtime目录本身。配置会在项目下次被请求时自动请求接口生成runtime
-     * @router delete runtime/:path[string]
+     *
+     * @return array [json]
+     * @throws \Exception
+     * @router delete runtime
      */
     public function deleteCache(Request $Request)
     {
-
+         $path = $Request->raw('path');
+        /**
+         * 判断是否有方法的目录
+         * 如 ../   ./
+         */
+        if(strpos($path,'..'.DIRECTORY_SEPARATOR) === 0 || strpos($path,'..'.DIRECTORY_SEPARATOR) > 0 ){
+            return $this->error([],'非法目录');
+        }
+        if(strpos($path,'.'.DIRECTORY_SEPARATOR) === 0 || strpos($path,'.'.DIRECTORY_SEPARATOR) > 0 ){
+            return $this->error([],'非法目录');
+        }
+        if($path ==='runtime')
+        {
+            $path = '..'.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR;
+        }else{
+            $path = '..'.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR.$path.DIRECTORY_SEPARATOR;
+        }
+        Func::M('file')::deldir($path);
     }
 
     /**
@@ -116,7 +141,8 @@ class Deploy extends Controller
      * @Author pizepei
      * @Created 2019/6/16 22:43
      * @param \pizepei\staging\Request $Request
-     * @return array [html]
+     * @return array [json]
+     *    data [raw]
      * @throws \Exception
      * @title  gitlab System hooks
      * @explain System hooks
@@ -124,9 +150,66 @@ class Deploy extends Controller
      */
     public function test(Request $Request)
     {
+        /**
+         * MicroServiceConfigCenterModel
+         */
+        //$MicroServiceConfigCenter = MicroServiceConfigCenterModel::table();
+        //$InitializeConfig = new InitializeConfig();
+        //$Config = $InitializeConfig->get_config_const('..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.__APP__.DIRECTORY_SEPARATOR);
+        //$dbtabase = $InitializeConfig->get_dbtabase_const('..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.__APP__.DIRECTORY_SEPARATOR);
+        //$error_log = $InitializeConfig->get_error_log_const('..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.__APP__.DIRECTORY_SEPARATOR);
+        //
+        //
+        //$deploy = $InitializeConfig->get_deploy_const('..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR);
+        //$data = [
+        //    'name'              =>'测试',
+        //    'appid'             =>$deploy['INITIALIZE']['appid'],
+        //    'service_group'     =>'develop',
+        //    'ip_white_list'     =>['47.106.89.196'],
+        //    'config'            =>$Config,
+        //    'dbtabase'          =>$dbtabase,
+        //    'error_or_log'      =>$error_log,
+        //    'deploy'            =>$deploy,
+        //    'domain'            =>$_SERVER['HTTP_HOST'],
+        //];
+        //
+        //return $MicroServiceConfigCenter->add($data);
+        $LocalDeployServic = new LocalDeployServic();
+        $data=[
+            'ProcurementType'=>'ErrorOrLogConfig',//获取类型   Config.php  Dbtabase.php  ErrorOrLogConfig.php
+            'appid'=>\Deploy::INITIALIZE['appid'],//项目标识
+            'domain'=>$_SERVER['HTTP_HOST'],//当前域名
+            'time'=>time(),//
+        ];
+        return $LocalDeployServic->getConfigCenter($data);
 
-        return ;
+    }
 
+    /**
+     * @Author pizepei
+     * @Created 2019/7/5 22:40
+     *
+     * @param \pizepei\staging\Request $Request
+     *      post [object] 路径参数
+     *          nonce [int required] 随机数
+     *          timestamp [string required]  时间戳
+     *          signature [string required] 签名
+     *          encrypt_msg [string required] 密文
+     *          domain [string required] 域名
+     *      path [object] 路径参数
+     *          appid [string] 项目appid
+     * @title  获取项目配置接口
+     * @explain 获取项目配置接口（基础配置）。
+     * @throws \Exception
+     * @return array [json]
+     *      data [raw]
+     * @router post service-config/:appid[string]
+     */
+    public function serviceConfig(Request $Request)
+    {
+
+        $LocalDeploy = new LocalDeployServic();
+        return $LocalDeploy->initConfigCenter($Request->post(),$Request->path('appid'));
     }
 
 }
