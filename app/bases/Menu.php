a3f979f66219285d40049aa2720b0abbed2d4480
皮泽培 <848733913@qq.com>
2019-07-29 18:09:12 +0800

    修复菜单问题

--- app/bases/Menu.php
+++ app/bases/Menu.php
@@ -12,6 +12,8 @@
 namespace app\bases;
 
 
+use model\basics\backstage\AdminMenuModel;
+use model\basics\backstage\MenuModel;
 use pizepei\staging\Controller;
 use pizepei\staging\Request;
 
@@ -23,6 +25,20 @@ class Menu extends Controller
      *
      * @param \pizepei\staging\Request $Request
      * @return array [json]
+     *      data [objectList]
+     *          name [string] 一级菜单名称（与视图的文件夹名称和路由路径对应）
+     *          title [string] 一级菜单标题
+     *          icon [string] 一级菜单图标样式
+     *          spread [bool] 是否默认展子菜单
+     *          list [objectList] 二级
+     *              name [string] 二级菜单名称（与视图的文件夹名称和路由路径对应）
+     *              title [string] 二级菜单标题
+     *              icon [string] 二级菜单图标样式
+     *              spread [bool] 是否默认展子菜单
+     *              list [objectList] 三级
+     *                  name [string] 三级菜单名称
+     *                  title [string] 三级菜单标题
+     *                  icon [string] 三级菜单图标样式
      * @title  获取菜单列表
      * @explain 获取菜单列表（权限不同内容不同）
      * @authTiny 微权限提供权限分配 [获取店铺所有  获取所有店铺  获取一个]
@@ -30,18 +46,53 @@ class Menu extends Controller
      * @authExtend UserExtend.list:删除账号操作
      * @baseAuth UserAuth:test
      * @router get menu-list
+     * @throws \Exception
      */
     public function index(Request $Request)
     {
+        AdminMenuModel::table()->fetchAll();
         return $this->succeed([
             [
-                'title'=>'工作台',
-                'icon'=>"layui-icon-home",
+                "name"=>"component",//一级菜单名称（与视图的文件夹名称和路由路径对应）
+                'title'=>'工作台',//一级菜单标题
+                'icon'=>"layui-icon-home",//一级菜单图标样式
+                "spread"=>true,//是否默认展子菜单（1.0.0-beta9 新增）
                 'list'=>[[
-                    'title'=>'导航',
-                    'jump'=>'/',
+                    "name"=> "grid", //二级菜单名称（与视图的文件夹名称和路由路径对应）
+                    'title'=>'导航',//二级菜单标题
+                    'jump'=>'/',//自定义一级菜单路由地址，默认按照 name 解析。一旦设置，将优先按照 jump 设定的路由跳转
+                    'list'=>null,
                 ]]
-            ]
+            ],
+            [
+            'title'=>'微信应用',
+            'icon'=>"layui-icon-home",
+                "name"=>"wechat",//一级菜单名称（与视图的文件夹名称和路由路径对应）
+                'list'=>[
+                    [
+                        "name"=> "Tencent", //二级菜单名称（与视图的文件夹名称和路由路径对应）
+                        'title'=>'微信公众号管理',//二级菜单标题
+                        'list'=>[
+                                    [
+                                    "name"=> "lsit", //二级菜单名称（与视图的文件夹名称和路由路径对应）
+                                    'title'=>'公众号列表',//二级菜单标题
+                                    'jump'=>'/',//自定义一级菜单路由地址，默认按照 name 解析。一旦设置，将优先按照 jump 设定的路由跳转
+                                    ],
+                                ]
+                    ],
+                    [
+                        "name"=> "codeAppManage", //二级菜单名称（与视图的文件夹名称和路由路径对应）
+                        'title'=>'微信验证应用管理',//二级菜单标题
+                        'list'=>[
+                                [
+                                "name"=> "applsit", //二级菜单名称（与视图的文件夹名称和路由路径对应）
+                                'title'=>'导航',//二级菜单标题
+                                'jump'=>'/',//自定义一级菜单路由地址，默认按照 name 解析。一旦设置，将优先按照 jump 设定的路由跳转
+                                ],
+                        ]
+                    ]
+                ]
+        ]
         ]);
 
     }
