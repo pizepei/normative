fb5fb3928e62fc0988477b0a8d972fe650b75b09
皮泽培 <848733913@qq.com>
2019-09-11 18:08:55 +0800

    ddns

---
+++ app/simpleIot/Ddns.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pizepei
+ * Date: 2019/09/11
+ * Time: 11:28
+ * @baseControl pizepei\simple-iot\src\controller\BasicsDdns
+ * @title 动态域名服务
+ * @authGroup [user:用户相关,admin:管理员相关] 权限组列表
+ * @basePath /simple-iot/ddns/
+ */
+namespace app\simpleIot;
+
+use pizepei\simpleIot\controller\BasicsDdns;
+
+class Ddns extends BasicsDdns
+{
+
+}
