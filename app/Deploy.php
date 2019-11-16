6928373fff3ff431d4507d01588e9a2f2da4ff8b
皮泽培 <848733913@qq.com>
2019-11-16 17:58:06 +0800

    修改注解

--- app/Deploy.php
+++ app/Deploy.php
@@ -4,13 +4,15 @@
  * User: pizepei
  * Date: 2019/4/22
  * Time: 14:40
- * @baseControl pizepei\deploy\src\controller\BasicsDeploy
- * @baseAuth DeployAuth:test
  * @title 部署控制器
  * @basePath /deploy/
+ * @baseAuth DeployAuth:test
+ * @packageName 系统部署
+ * @packageAuthor pizepei
+ * @basePermissions systemDeploy:系统部署
+ * @baseControl pizepei\deploy\src\controller\BasicsDeploy
  */
 namespace app;
-
 
 use pizepei\deploy\controller\BasicsDeploy;
 
