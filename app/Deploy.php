52f235ea6c5d2d607b4900c8bef290ffa3d36846
pi <pizepei@pizepei.com>
2019-12-03 22:45:49 +0800

    自动更新的控制器文件

--- app/Deploy.php
+++ app/Deploy.php
@@ -2,22 +2,27 @@
 /**
  * Created by PhpStorm.
  * User: pizepei
- * Date: 2019/4/22
- * Time: 14:40
+ * Date: 2019-12-03
+ * Time: 21:47:51
  * @title 部署控制器
  * @basePath /deploy/
- * @baseAuth DeployAuth:test
+ * @baseAuth UserAuth:test
+ * @baseAuthGroup systemDeploy:系统部署
  * @packageName 系统部署
  * @packageAuthor pizepei
- * @basePermissions systemDeploy:系统部署
+ * @baseParam [$Request:pizepei\staging\Request]
  * @baseControl pizepei\deploy\src\controller\BasicsDeploy
  */
+ 
+declare(strict_types=1);
+
 namespace app;
 
-use pizepei\deploy\controller\BasicsDeploy;
+use \pizepei\deploy\controller\BasicsDeploy;
 
 class Deploy extends BasicsDeploy
 {
 
-
 }
+
+    
